deaa7d1bf07bef217b97b40b48efb4a245bc148d
Chochlik <73795200+Ch0chl1k@users.noreply.github.com>
2025-04-09 20:02:11 +0200

    Update MainGUI.java

--- MainGUI.java
+++ MainGUI.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 public class MainGUI {
     public static void MainGUI() {
 
-        // Utworzenie okana
         JFrame frame = new JFrame("Fortis Bank");
         frame.setSize(400, 230);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,7 +13,6 @@ public class MainGUI {
         frame.setLayout(new BorderLayout());
         frame.getContentPane().setBackground(new Color(192,192,192));
 
-        // Napis i te inne bajery
         JLabel welcome = new JLabel("Welcome to the Fortis Bank", SwingConstants.CENTER);
         welcome.setFont(new Font("Arial", Font.BOLD, 20));
         welcome.setForeground(Color.CYAN);
@@ -27,12 +25,8 @@ public class MainGUI {
         connect.setForeground(Color.CYAN);
 
         frame.add(connect);
-
-
-
-        // dodanei guzika (panel)
+        
         JPanel buttonPanel = new JPanel();
-        // ustwinie guzika
         buttonPanel.setOpaque(false);
         buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 60, 30));
         JButton loginButton = new JButton("Login");
@@ -40,8 +34,7 @@ public class MainGUI {
 
         loginButton.setBackground(new Color(169,169,169));
         registerButton.setBackground(new Color(169,169,169));
-
-        // przekierowanie do innej kalsy
+        
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -60,16 +53,11 @@ public class MainGUI {
             }
         });
 
-        // pokaznie na stronie
         buttonPanel.add(loginButton);
         buttonPanel.add(registerButton);
 
-
-
-        // guzik pozycja
         frame.add(buttonPanel, BorderLayout.SOUTH);
 
-        // pokazanie calego boxa itp
         frame.setVisible(true);
     }
 }
